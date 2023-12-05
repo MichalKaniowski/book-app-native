e70bfdf4a4a11a76b0436a87910e78aaa68a5c1f
Michał Kaniowski <kaniowskimichal2@gmail.com>
2023-12-05 22:14:10 +0100

    style: add theme button

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -9,13 +9,20 @@ import StyledText from "../components/ui/StyledText";
 import { useContext } from "react";
 import { ThemeContext } from "../store/ThemeContext";
 import Icon from "react-native-vector-icons/Fontisto";
+import { ThemeType } from "../types/theme";
+import FeatherIcon from "react-native-vector-icons/Feather";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function Profile() {
-  const { theme } = useContext(ThemeContext);
+  const { theme, onThemeChange, themeValue } = useContext(ThemeContext);
 
   // mocked data
   const stats: string[] = [];
   const badges: string[] = [];
+
+  function handleThemeChange(theme: ThemeType) {
+    onThemeChange(theme);
+  }
 
   return (
     <ScrollView style={{ backgroundColor: theme.background, padding: 10 }}>
@@ -51,28 +58,59 @@ export default function Profile() {
         <View style={styles.profileSettingsBox}>
           <View style={{ ...styles.border, borderColor: theme.secondary }} />
           <StyledText style={styles.profileSettingsText}>
-            Zrecenzuj naszą aplikację
-          </StyledText>
-        </View>
-
-        <View style={styles.profileSettingsBox}>
-          <View style={{ ...styles.border, borderColor: theme.secondary }} />
-          <StyledText style={styles.profileSettingsText}>
-            Skonaktuj się z nami
-          </StyledText>
-        </View>
-
-        <View style={styles.profileSettingsBox}>
-          <View style={{ ...styles.border, borderColor: theme.secondary }} />
-          <StyledText style={styles.profileSettingsText}>
             Zmień język
           </StyledText>
         </View>
         <View style={styles.profileSettingsBox}>
           <View style={{ ...styles.border, borderColor: theme.secondary }} />
-          <StyledText style={styles.profileSettingsText}>
-            Zmień motyw kolorów
-          </StyledText>
+          <View style={styles.boxWithTextAndIcon}>
+            <StyledText style={styles.profileSettingsText}>
+              Zmień motyw kolorów
+            </StyledText>
+            <View
+              style={{
+                ...styles.themeContainer,
+                borderColor: theme.text,
+              }}
+            >
+              <TouchableOpacity
+                onPress={() => handleThemeChange("light")}
+                style={{
+                  ...styles.themeButton,
+                  backgroundColor:
+                    themeValue === "light" ? theme.secondary : "#000",
+                }}
+              >
+                <FeatherIcon name="sun" color={theme.text} size={20} />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => handleThemeChange("dark")}
+                style={{
+                  ...styles.themeButton,
+                  backgroundColor:
+                    themeValue === "dark" ? theme.secondary : theme.background,
+                }}
+              >
+                <FeatherIcon name="moon" color={theme.text} size={20} />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => handleThemeChange("system")}
+                style={{
+                  ...styles.themeButton,
+                  backgroundColor:
+                    themeValue === "system"
+                      ? theme.secondary
+                      : theme.background,
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="theme-light-dark"
+                  size={20}
+                  color={theme.text}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
         <View style={styles.profileSettingsBox}>
           <View style={{ ...styles.border, borderColor: theme.secondary }} />
@@ -129,6 +167,22 @@ const styles = StyleSheet.create({
   boxWithTextAndIcon: {
     flexDirection: "row",
     gap: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  themeContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    overflow: "hidden",
+    position: "relative",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1,
+  },
+  themeButton: {
+    paddingVertical: 2,
+    paddingHorizontal: 10,
   },
   border: {
     borderTopWidth: 0.5,
