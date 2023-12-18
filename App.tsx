4d3479859fea228c4aa370f790ef1130739fba17
MichalKaniowski <kaniowskimichal2@gmail.com>
2023-12-18 21:48:28 +0100

    feat: add tracking of spent time by user in application

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
 import "react-native-gesture-handler";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useRef, useCallback } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { User, onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from "./FirebaseConfig";
 import Login from "./src/screens/Login";
-import { StatusBar, SafeAreaView } from "react-native";
+import { StatusBar, SafeAreaView, AppStateStatus } from "react-native";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { CONVEX_URL } from "@env";
+import { CONVEX_URL, DOMAIN } from "@env";
 import { ThemeContext, ThemeContextProvider } from "./src/store/ThemeContext";
 import MainTabNavigator from "./src/routes/MainTabNavigator";
 import { TabsContextProvider } from "./src/store/TabsContext";
 import { BookContextProvider } from "./src/store/BookContext";
+import { AppState } from "react-native";
+import { differenceInSeconds } from "date-fns";
+import axios from "axios";
 
 const Stack = createNativeStackNavigator();
 
@@ -24,11 +27,25 @@ function AppContent() {
 
   const { theme, actualTheme } = useContext(ThemeContext);
 
+  let appStartTime = new Date();
+
   useEffect(() => {
     onAuthStateChanged(firebaseAuth, (user) => {
       setUser(user);
     });
+    AppState.addEventListener("change", handleAppStateChange);
   }, []);
+
+  const handleAppStateChange = useCallback(async(nextAppState: AppStateStatus) => {
+    if (nextAppState === "background" || nextAppState === "inactive") {
+      const viewSessionDuration = Math.round(((new Date()).getTime() / 1000) - (appStartTime.getTime()/1000));
+
+      await axios.post(`${DOMAIN}/api/users/addSpentTime`, {userFirebaseId: firebaseAuth.currentUser?.uid, spentTimeInSeconds: viewSessionDuration})
+
+    } else {
+      appStartTime = new Date();
+    }
+  }, [])
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: theme.background }}>
